c006d55ceac0834b2744243ec685d99d31f7c3cc
mane dinesh <manedinesh650@gmail.com>
2026-05-13 18:05:45 +0530

    changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -470,6 +470,7 @@ const Deloitte = "/Deloitte.png";
 const FYMarksheet = "/FYBCS.pdf";
 const SYMarksheet = "/SY.BCS.pdf";
 const TYMarksheet = "/TYBCS.pdf";
+const Pic = "/Dinesh.jpg";
 
 function Home() {
   const [showCertModal, setShowCertModal] = useState(false);
